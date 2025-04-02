2bed640822bff35371dccda18cb1f740fde2dd54
Rafael Maia <rafael7maia@gmail.com>
2025-04-02 07:37:41 -0300

    Update setup_pyenv.sh

--- setup_pyenv.sh
+++ setup_pyenv.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
+
+# Nome do ambiente virtual
+VENV_NAME="meu_ambiente_virtual"
+
+# Cria o ambiente virtual
+python -m venv $VENV_NAME
+
+# Ativa o ambiente virtual
+source $VENV_NAME/bin/activate
+
+# Instala as dependências necessárias
+pip install requests beautifulsoup4 dirsearch-py
+
+# Exibe uma mensagem de confirmação
+echo "Ambiente virtual '$VENV_NAME' criado e ativado."
+echo "Dependências instaladas."
+echo "Você pode agora executar o script 'scan_web.py'."
 
